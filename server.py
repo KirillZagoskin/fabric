b14aac9ad59125b2af72af065c617f13d4fcdab1
Kirill Zagoskin <kirillzagoskin.work@gmail.com>
2021-12-28 12:47:42 +0300

    First version

---
+++ server.py
@@ -0,0 +1,10 @@
+from fastapi import FastAPI
+from fastapi.responses import Response
+
+
+app = FastAPI()
+
+@app.get('/')
+def main_page():
+    response = Response('<h1>Hello, World!</h1>', media_type='text/html')
+    return response
